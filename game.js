1f755362906308395694dd8e62c4b6f057a31590
fannymoeremans-netizen <fannymoeremans@gmail.com>
2026-09-08 14:07:59 +0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,38 @@
+function mulaiGame(){
+
+document.getElementById("menu")
+.style.display="none";
+
+
+document.getElementById("map")
+.style.display="block";
+
+}
+
+
+
+function guru(){
+
+alert(
+"👨‍🏫 Guru Pecahan:\nSelamat datang di Hutan Pecahan!"
+);
+
+}
+
+
+function monster(){
+
+alert(
+"👾 Monster Penyebut:\nJawab soal matematika untuk mengalahkanku!"
+);
+
+}
+
+
+function kristal(){
+
+alert(
+"💎 Kamu mendapatkan Kristal Pembilang!"
+);
+
+}
